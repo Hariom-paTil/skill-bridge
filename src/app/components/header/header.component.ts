72febeae4b0f5b0d93dd332a46ad55789cec1ce7
hariompatil <hariompatil757@gmail.com>
2026-01-13 16:51:52 +0530

    Commit 17: connect the header "Top Apps" button to the top apps section.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, inject, OnInit } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
 import { AuthModalService } from '../../services/auth-modal.service';
 import { AuthStateService } from '../../services/auth-state.service';
 
@@ -12,6 +13,7 @@ import { AuthStateService } from '../../services/auth-state.service';
 export class HeaderComponent implements OnInit {
   authModal = inject(AuthModalService);
   authState = inject(AuthStateService);
+  viewportScroller = inject(ViewportScroller);
   showProfileDropdown = false;
 
   ngOnInit() {
@@ -37,5 +39,15 @@ export class HeaderComponent implements OnInit {
       return (details.firstName.charAt(0) + details.lastName.charAt(0)).toUpperCase();
     }
     return this.authState.userName().charAt(0).toUpperCase();
+  }
+
+  scrollToTopApps() {
+    this.viewportScroller.scrollToPosition([0, 0]);
+    setTimeout(() => {
+      const element = document.getElementById('top-apps-section');
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+    }, 100);
   }
 }
